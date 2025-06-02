3fad8bdd6556889b83d5242f94d1dac482934778
CDA <CDA@STATION-Uo8g2QJ.exchange.ad.afpanet>
2025-06-02 11:05:52 +0200

    initial commit

---
+++ layout/layout.js
@@ -0,0 +1,19 @@
+function loadHTML(idContainer, urlHTML, urlCSS) {
+  fetch(urlHTML)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(idContainer).innerHTML = html;
+      if (urlCSS) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = urlCSS;
+        document.head.appendChild(link);
+      }
+    })
+    .catch(e => console.error(`Erreur chargement ${urlHTML} :`, e));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadHTML('nav-container', '/layout/navbar/navbar.html', '/layout/navbar/navbar.css');
+  loadHTML('footer-container', '/layout/footer/footer.html', '/layout/footer/footer.css');
+});
